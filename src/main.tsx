045cf1fd7cba098ece6bc2186a3539ab7d515a20
Abhishek Manjunath <abhishek@phdata.io>
2025-12-27 20:46:49 +0530

    chore: add version log

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log(`DataGym Version: ${__APP_VERSION__}`);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
